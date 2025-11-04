07cdb17847bb46bdbb29ce82eefd55e42b74b8c3
da408711 <da408711@ucf.edu>
2025-11-04 15:22:25 -0500

    The Start

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform targetTransform; //The object the camera will follow
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+
+    public float cameraFollowSpeed = 0.2f;
+
+    private void Awake()
+    {
+        targetTransform = Object.FindFirstObjectByType<Player>().transform;
+    }
+    public void FollowTarget()
+    {
+        Vector3 targetPosition = Vector3.SmoothDamp
+            (transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
+
+        transform.position = targetPosition;
+    } 
+}
